9c3872c1ac52efeea738da1a1273ef2167f81484
RobinU434 <robin.uhrich@arcor.de>
2022-04-28 17:09:51 +0200

    changed to 4 space layout

--- locomotion_custom/examples/test_env_gui.py
+++ locomotion_custom/examples/test_env_gui.py
@@ -29,38 +29,38 @@ MOTOR_CONTROL_MODE_MAP = {
 
 
 def main(_):
-  robot = ROBOT_CLASS_MAP[FLAGS.robot_type]
-  motor_control_mode = MOTOR_CONTROL_MODE_MAP[FLAGS.motor_control_mode]
-  env = env_builder.build_regular_env(robot,
-                                      motor_control_mode=motor_control_mode,
-                                      enable_rendering=True,
-                                      on_rack=FLAGS.on_rack,
-                                      wrap_trajectory_generator=False)
+    robot = ROBOT_CLASS_MAP[FLAGS.robot_type]
+    motor_control_mode = MOTOR_CONTROL_MODE_MAP[FLAGS.motor_control_mode]
+    env = env_builder.build_regular_env(robot,
+                                        motor_control_mode=motor_control_mode,
+                                        enable_rendering=True,
+                                        on_rack=FLAGS.on_rack,
+                                        wrap_trajectory_generator=False)
 
-  action_low, action_high = env.action_space.low, env.action_space.high
-  action_median = (action_low + action_high) / 2.
-  dim_action = action_low.shape[0]
-  action_selector_ids = []
-  for dim in range(dim_action):
-    action_selector_id = p.addUserDebugParameter(paramName='dim{}'.format(dim),
-                                                 rangeMin=action_low[dim],
-                                                 rangeMax=action_high[dim],
-                                                 startValue=action_median[dim])
-    action_selector_ids.append(action_selector_id)
-
-  if FLAGS.video_dir:
-    log_id = p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, FLAGS.video_dir)
-
-  for _ in tqdm(range(800)):
-    action = np.zeros(dim_action)
+    action_low, action_high = env.action_space.low, env.action_space.high
+    action_median = (action_low + action_high) / 2.
+    dim_action = action_low.shape[0]
+    action_selector_ids = []
     for dim in range(dim_action):
-      action[dim] = env.pybullet_client.readUserDebugParameter(
-          action_selector_ids[dim])
-    env.step(action)
+        action_selector_id = p.addUserDebugParameter(paramName='dim{}'.format(dim),
+                                                     rangeMin=action_low[dim],
+                                                     rangeMax=action_high[dim],
+                                                     startValue=action_median[dim])
+        action_selector_ids.append(action_selector_id)
 
-  if FLAGS.video_dir:
-    p.stopStateLogging(log_id)
+    if FLAGS.video_dir:
+        log_id = p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, FLAGS.video_dir)
+
+    for _ in tqdm(range(800)):
+        action = np.zeros(dim_action)
+        for dim in range(dim_action):
+            action[dim] = env.pybullet_client.readUserDebugParameter(
+                action_selector_ids[dim])
+        env.step(action)
+
+    if FLAGS.video_dir:
+        p.stopStateLogging(log_id)
 
 
 if __name__ == "__main__":
-  app.run(main)
+    app.run(main)
