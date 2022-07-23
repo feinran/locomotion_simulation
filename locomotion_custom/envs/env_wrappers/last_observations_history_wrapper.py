86c8c937068145534615d53da910cbe225b75587
Felix Schulz <uni@feli.xyz>
2022-07-23 21:18:48 +0200

    flattend last_observations_history_wrapper

--- locomotion_custom/envs/env_wrappers/last_observations_history_wrapper.py
+++ locomotion_custom/envs/env_wrappers/last_observations_history_wrapper.py
@@ -4,20 +4,20 @@ import numpy as np
 
 class LastObservationsHistoryWrapper(gym.ObservationWrapper):
   """An env wrapper that keeps the last couple of observations."""
-  def __init__(self, gym_env, n_observations: int = 4):
+  def __init__(self, gym_env, n_observations: int):
     super(LastObservationsHistoryWrapper, self).__init__(gym_env)
     self._observations_history = None
     self._n_observations = n_observations
 
-    low = np.array([self.observation_space.low] * n_observations)
-    high = np.array([self.observation_space.high] * n_observations)
+    low = np.array(list(self.observation_space.low) * n_observations)
+    high = np.array(list(self.observation_space.high) * n_observations)
     self.observation_space = gym.spaces.Box(low, high)
 
   def observation(self, new_observations):
     if self._observations_history is None:
-        self._observations_history = np.zeros((self._n_observations, new_observations.shape[0]))
+        self._observations_history = np.zeros(new_observations.shape[0] * self._n_observations)
 
-    self._observations_history = np.concatenate([self._observations_history, [new_observations]])[1:]
+    self._observations_history = np.concatenate([self._observations_history, new_observations])[new_observations.shape[0]:]
 
     return self._observations_history
 
