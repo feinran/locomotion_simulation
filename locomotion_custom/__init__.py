74aef164dd6406aef06f7d5504513374c950bb38
Felix Schulz <uni@feli.xyz>
2022-07-24 12:03:11 +0200

    halfed max_epsiode_steps

--- locomotion_custom/__init__.py
+++ locomotion_custom/__init__.py
@@ -13,6 +13,6 @@ def register(env_id, *args, **kvargs):
 register(
     env_id='custom-v1',
     entry_point='locomotion_simulation.locomotion_custom.envs.gym_envs:A1GymEnvCustom',
-    max_episode_steps=2000,
+    max_episode_steps=1000,
     reward_threshold=2000.0,
 )
